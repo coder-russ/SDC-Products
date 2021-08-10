4f3b7975a1b936953c3d7ada6116b15ccec15f80
Russ Lightsey <texanruss3@gmail.com>
2021-08-09 20:57:06 -0700

    server is running, installed nodemon, airbnb eslint, updated start script

--- Server/app.js
+++ Server/app.js
@@ -1,1 +1,11 @@
-//your code here
+const express = require('express');
+
+const app = express();
+const port = 3000;
+const router = require('./routes');
+
+app.use(express.json());
+
+app.use('/', router);
+
+app.listen(port, () => console.log('Server listening on port ', port));
