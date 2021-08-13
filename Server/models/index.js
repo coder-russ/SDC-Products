f1310c243140ab615894961506bb155ddfce835f
Russ Lightsey <texanruss3@gmail.com>
2021-08-12 19:06:42 -0700

    completed get product id route and method

--- Server/models/index.js
+++ Server/models/index.js
@@ -4,8 +4,26 @@ const { initModels } = require('./tables/init-models');
 const models = initModels(sequelize);
 
 module.exports = {
-  getProducts: () => models.product.findAll({ limit: 10 }),
-  getProductId: () => 'placeholder',
+  getProducts: (query) => {
+    const page = query.page || 1;
+    const count = query.count || 5;
+    const total = Number(page) * Number(count);
+    return models.product.findAll({ limit: total });
+  },
+  getProductId: (productId) => {
+    const promise1 = models.product.findAll({
+      where: {
+        id: productId,
+      },
+    });
+    const promise2 = models.features.findAll({
+      attributes: ['feature', 'value'],
+      where: {
+        product_id: productId,
+      },
+    });
+    return Promise.all([promise1, promise2]);
+  },
   getProductStyles: () => 'placeholder',
   getProductRelated: () => 'placeholder',
 };
--- Server/routes.js
+++ Server/routes.js
@@ -2,14 +2,25 @@ const router = require('express').Router();
 const db = require('./models/index');
 
 router.get('/products', (req, res) => {
-  db.getProducts()
+  db.getProducts(req.query)
     .then((data) => res.send(data))
     .catch((err) => res.send(err));
 });
 
 router.get('/products/:id', (req, res) => {
-  db.getProductId()
-    .then((data) => res.send(data))
+  db.getProductId(req.params.id)
+    .then((array) => {
+      const result = {
+        id: array[0][0].dataValues.id,
+        name: array[0][0].dataValues.name,
+        slogan: array[0][0].dataValues.slogan,
+        description: array[0][0].dataValues.description,
+        category: array[0][0].dataValues.category,
+        default_price: array[0][0].dataValues.default_price,
+        features: array[1],
+      };
+      res.send(result);
+    })
     .catch((err) => res.send(err));
 });
 
